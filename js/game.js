db37b1428e0589e40c51aaddd91783c93b2e791d
it1926 <janslivka55@seznam.cz>
2020-10-19 18:11:11 +0200

    Vychozi verze

--- js/game.js
+++ js/game.js
@@ -1,10 +1,41 @@
 var hody = [];
 var timer = false;
 
+const buttonj1 = document.getElementById("buttonj1");
+const buttonj2 = document.getElementById("buttonj2");
+const buttonj3 = document.getElementById("buttonj3");
+const buttonj4 = document.getElementById("buttonj4");
 
+     buttonj1.addEventListener("click", function(){
+    let jmeno1 = prompt( "Zadej cele jmeno", "hráč");
+    console.log (`Zadané jméno je ${jmeno1}  `);
+    document.getElementById('jmeno1').innerHTML = `<h1>${jmeno1}</h1>` ;
+    
+  })   
+  
+  buttonj2.addEventListener("click", function(){
+    let jmeno2 = prompt( "Zadej cele jmeno", "hráč");
+    console.log (`Zadané jméno je ${jmeno2} `);
+    document.getElementById('jmeno2').innerHTML = `<h1>${jmeno2}</h1>` ;
+    
+  })   
+  buttonj3.addEventListener("click", function(){
+    let jmeno3 = prompt( "Zadej cele jmeno", "hráč");
+    console.log (`Zadané jméno pro hráče 3 je ${jmeno3} `);
+    document.getElementById('jmeno3').innerHTML = `<h1>${jmeno3}</h1>` ;
+    
+  })   
+  buttonj4.addEventListener("click", function(){
+    let jmeno4 = prompt( "Zadej cele jmeno", "hráč");
+    console.log (`Zadané jméno je${jmeno4}  `);
+    document.getElementById('jmeno4').innerHTML =  `<h1>${jmeno4}</h1>` ;
+    
+  })
+  
 function animace(){
     var h = Math.ceil(Math.random() * 6);
     document.getElementById('cube').src='img/kostka' + h +  '.png';
+    document.getElementById('jmeno').innerHTML = '<p>Hod: ' + h + '</p>';
 }
 
 document.getElementById('game').addEventListener('click',
@@ -52,6 +83,8 @@ function average(sum, count) {
     return (sum / count).toFixed(2);
 }
 
+
+
 function hod() {
     var h = Math.ceil(Math.random() * 6);
     hody.push(h);
@@ -59,13 +92,25 @@ function hod() {
     document.getElementById('result').innerHTML = '<p>Hod: ' + h + '</p>';
     document.getElementById('result').innerHTML += 
         '<p>Počet hodů: ' + hody.length + '</p>';
-    document.getElementById('result').innerHTML += 
-        '<p>Součet hodů: ' + suma(hody) + '</p>';
+
+        if (suma(hody)<=50){
+            document.getElementById('result').innerHTML += 
+    
+            '<p>Součet hodů: ' + suma(hody) + '</p>';
+        }
+
+        else if (suma(hody)>=50){
+            console.log("Uz bylo 50 a funguje to");
+            alert(`Dnes je ${j}`);
+
+        }
+    
+    
     document.getElementById('result').innerHTML += 
         '<p>Průměr hodů: ' + average(suma(hody),hody.length) + '</p>';
     document.getElementById('result').innerHTML += 
         '<p>Nejvyšší hod: ' + maximum(hody) + '</p>';
     document.getElementById('result').innerHTML += 
-        '<p>Nejvyšší hod: ' + minimum(hody) + '</p>';
+        '<p>Nejnižší hod: ' + minimum(hody) + '</p>';
     return h;
 }
